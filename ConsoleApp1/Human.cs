332e81ab949745fc5a5cd20dd8870304de14c158
shaikshoaib99 <smdshoaib137@gmail.com>
2023-05-05 12:59:39 +0530

    [Shoaib] Adding Human.cs

---
+++ ConsoleApp1/Human.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+
+{
+    //access-modifier class-keyword className
+    internal class Human
+    {
+        //instance variables
+        //access-modifier datatype variableName
+        public string name;
+        public float height;
+        public long phoneNumber;
+
+        //access-modifier returntype methodName
+        public void Talk ()
+        {
+            Console.WriteLine("Human can talk");
+        }
+        public void HumanDetails()
+        {
+            Console.WriteLine("name:{0} height:{1} phone {2}", name, height, phoneNumber);
+        }
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,5 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello Earth");
-Console.WriteLine("Local Changes");
-Console.WriteLine("Conflict creation Sample");
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            /*Console.WriteLine("Hello Earth");
+            Console.WriteLine("Local Changes");
+            Console.WriteLine("Conflict creation Sample");*/
+            //object creatio of human class
+            //className objectCariable=newClassName();
+            Human human = new Human();
+            Console.WriteLine("Please enter your name");
+            human.name = Console.ReadLine();
+            Console.WriteLine("Please enter your height");
+            //string height = Console.ReadLhine();
+            human.height = Convert.ToSingle(Console.ReadLine());
+            human.phoneNumber = Convert.ToInt64(Console.ReadLine());
+            human.Talk();
+            human.HumanDetails();
+            Console.ReadLine();
+        }
+    }
+}
